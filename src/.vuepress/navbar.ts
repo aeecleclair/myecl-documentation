f431be26704a90ada5a047cdecaf0c98db22a82a
Marc-Andrieu <marc.andrieu@outlook.com>
2025-09-13 20:11:03 +0200

    Feat: personalized theme

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,10 +2,13 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
+  "/git/",
   "/hyperion/",
+  "/titan/",
+  "/metadocs/",
   {
     text: "Github",
     icon: "github",
-    link: "https://github.com/orgs/aeecleclair/",
+    link: "https://github.com/aeecleclair/",
   },
 ]);
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,14 +1,3 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export default sidebar({
-  "/": [
-    "",
-    {
-      text: "Hyperion",
-      icon: "laptop-code",
-      prefix: "hyperion/",
-      link: "hyperion/",
-      children: "structure",
-    },
-  ],
-});
+export default sidebar("structure");
